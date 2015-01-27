3759e7891dde7a6dfe87ab149c3e6760f6ae19c5
admangum <adam.mangum@autodesk.com>
2015-01-26 23:55:02 -0800

    Adds ability to "spread" arguments to callbacks

--- src/xhrfacade.js
+++ src/xhrfacade.js
@@ -7,6 +7,10 @@
             return typeof obj === 'string';
         }
 
+        function isArray(obj){
+            return obj instanceof Array;
+        }
+
         function isRegExp(obj) {
             return obj instanceof RegExp;
         }
@@ -26,32 +30,37 @@
             return obj;
         }
 
-        function getEndpointOptions(endpoints, name) {
-            var endpoint = name && endpoints[name];
+        function getEndpointOptions(endpoints, name, type) {
+            var endpoint;
+            type = type || 'GET';
+            endpoint = name && endpoints[name + type];
             return endpoint && endpoint.options || {};
         }
 
-        function getEndpointCache(endpoints, name) {
-            var endpoint = name && endpoints[name];
+        function getEndpointCache(endpoints, name, type) {
+            var endpoint;
+            type = type || 'GET';
+            endpoint = name && endpoints[name + type];
             return endpoint && endpoint.cache || null;
         }
 
-        function setEndpointOptions(endpoints, name, options) {
-            var endpoint;
-            if (!name) return;
-            endpoint = name && endpoints[name] || {};
-            if (options)
-                endpoint.options = options;
-            endpoints[name] = endpoint;
+        function setEndpointOptions(endpoints, name, type, options) {
+            var endpoint, id;
+            if (!name || !type) return;
+            id = name + type;
+            endpoint = endpoints[id] || {};
+            endpoint.options = options;
+            endpoints[id] = endpoint;
         }
 
-        function setEndpointCache(endpoints, name, cache) {
-            var endpoint;
-            if (!name) return;
-            endpoint = name && endpoints[name] || {};
-            if (cache)
+        function setEndpointCache(endpoints, name, type, cache) {
+            var endpoint, id;
+            if (!name || !type) return;
+            id = name + type;
+            endpoint = name && endpoints[id] || {};
+            if (endpoint && cache)
                 endpoint.cache = cache;
-            endpoints[name] = endpoint;
+            endpoints[id] = endpoint;
         }
 
         function useCache(options1, options2) {
@@ -104,54 +113,70 @@
             this.server = server;
         }
 
+        XhrFacade.REQUEST_ARRAY_REQUIRED = 'You must pass a request array to XhrFacade.get() method.';
+        XhrFacade.REGEXP_ENDPOINT_URL_REQUIRED = 'Requests to endpoints registered as regular expresions must provide a URLs.';
+        XhrFacade.ENDPOINT_NAME_REQUIRED = 'You must provide a name when adding an endpoint.';
+        XhrFacade.ENDPOINT_URL_REQUIRED = 'You must provide a URL when adding an endpoint.';
+
         XhrFacade.prototype.add = function(options) {
             var endpoints = this.endpoints;
             options = options || {};
             if (!options.name || !isString(options.name))
-                throw new Error('Cannot create endpoint without name');
+                throw new Error(XhrFacade.ENDPOINT_NAME_REQUIRED);
             if (!options.url || (!isString(options.url) && !isRegExp(options.url)))
-                throw new Error('Cannot create endpoint without url');
-            setEndpointOptions(endpoints, options.name, options);
+                throw new Error(XhrFacade.ENDPOINT_URL_REQUIRED);
+
+            options.type = options.type || 'GET';
+            setEndpointOptions(endpoints, options.name, options.type, options);
+
             if(options.response)
-                this.server.respondWith(options.type || 'GET', options.url, options.response);
+                this.server.respondWith(options.type, options.url, options.response);
         };
 
         XhrFacade.prototype.get = function() {
             var deferreds = [],
                 deferred,
-                requests = [].slice.call(arguments),
-                requestsLength = requests.length,
+                requests,
+                requestsLength,
                 request,
+                defaults,
                 options,
                 cache;
 
+            if(!arguments.length || !isArray(arguments[0]))
+                throw new Error(XhrFacade.REQUEST_ARRAY_REQUIRED);
+
+            requests = arguments.length ? arguments[0] : [];
+            requestsLength = requests.length;
+
             for (var i = 0; i < requestsLength; i++) {
-                requests[i] = isString(requests[i]) ? {
+                request = isString(requests[i]) ? {
                     name: requests[i]
                 } : requests[i];
-                request = requests[i];
 
-                options = getEndpointOptions(this.endpoints, request.name);
-                options = extend({}, options, request);
+                defaults = getEndpointOptions(this.endpoints, request.name, request.type);
+                options = extend({}, defaults, request);
 
-                cache = getEndpointCache(this.endpoints, options.name);
+                if(isRegExp(options.url))
+                    throw new Error(XhrFacade.REGEXP_ENDPOINT_URL_REQUIRED);
 
-                if (cache && useCache(options, request)) {
+                cache = getEndpointCache(this.endpoints, options.name, options.type);
+
+                if (cache && useCache(defaults, options)) {
                     deferred = cache;
                 } else if (options.url) {
-                    setEndpointOptions(this.endpoints, options.name, options);
                     deferred = $.ajax(options);
-                    setEndpointCache(this.endpoints, options.name, deferred);
+                    setEndpointCache(this.endpoints, options.name, options.type, deferred);
 
                 } else {
-                    deferred = null;
+                    deferred = requests[i];
                 }
                 deferreds.push(deferred);
             }
 
-            return new RSVP.Promise(function(resolve, reject) {
-                $.when.apply($, deferreds).then(resolve, reject);
-            });
+            deferreds = deferreds.concat(Array.prototype.slice.call(arguments, 1));
+
+            return RSVP.all(deferreds);
         };
 
         XhrFacade.prototype.restore = function() {
--- test/spec/xhrfacade.spec.js
+++ test/spec/xhrfacade.spec.js
@@ -11,11 +11,24 @@
             sinon.spy(jQuery, 'ajax');
             facade = new XhrFacade();
             facade.add({
-                'name': 'virtual-service',
-                'url': '/virtual-service',
-                'response': function(request){
+                'name': 'hola',
+                'url': '/test/data/hola.json'
+            });
+            facade.add({
+                'name': 'bonjour',
+                'url': '/bonjour',
+                'response': function(request, id){
                     request.respond(JSON.stringify({
-                        message: "I'm a virtual response!"
+                        message: 'bonjour!'
+                    }));
+                }
+            });
+            facade.add({
+                'name': 'custom',
+                'url': /\/custom\/(.+)/,
+                'response': function(request, greeting){
+                    request.respond(JSON.stringify({
+                        message: greeting
                     }));
                 }
             });
@@ -29,10 +42,13 @@
         it('should be a constructor function', function() {
             expect(facade instanceof XhrFacade).to.be.true;
         });
+
         describe('.add()', function() {
+
             it('should be a function', function() {
                 expect(facade.add).to.be.a('function');
             });
+
             it('should throw error if name is not provided for endpoint', function(){
                 var err = {};
                 try{
@@ -40,9 +56,10 @@
                 }catch(e){
                     err = e;
                 }finally{
-                    expect(err.message).to.equal('Cannot create endpoint without name');
+                    expect(err.message).to.equal(XhrFacade.ENDPOINT_NAME_REQUIRED);
                 }
             });
+
             it('should throw error if url is not provided for endpoint', function(){
                 var err = {};
                 try{
@@ -50,64 +67,123 @@
                 }catch(e){
                     err = e;
                 }finally{
-                    expect(err.message).to.equal('Cannot create endpoint without url');
+                    expect(err.message).to.equal(XhrFacade.ENDPOINT_URL_REQUIRED);
                 }
             });
+
             it('should allow real endpoints to be called by name', function(done){
-                facade.add({
-                    'name': 'hola',
-                    'url': '/test/data/hola.json'
-                });
-                facade.get('hola')
-                    .then(function(response){
-                        expect(response.message).to.equal('hola!');
+                facade.get(['hola'])
+                    .then(function(responses){
+                        expect(responses[0].message).to.equal('hola!');
                         done();
                     });
             });
+
             it('should allow virtual endpoints to be called by name', function(done){
+                facade.get([{
+                    'name': 'bonjour'
+                }])
+                .then(function(responses){
+                    expect(responses[0].message).to.equal('bonjour!');
+                    done();
+                });
+            });
+
+            it('should allow urls to be specified as regular expressions with capture groups', function(done){
+                facade.get([{
+                    'name': 'custom',
+                    'url': '/custom/aloha!'
+                }])
+                .then(function(responses){
+                    expect(responses[0].message).to.equal("aloha!");
+                    done();
+                });
+            });
+
+            it('should register seperate default options for each endpoint HTTP method', function(done){
                 facade.add({
-                    'name': 'bonjour',
-                    'url': '/bonjour',
+                    'name': 'method-man',
+                    'url': '/method-man',
+                    'type': 'GET',
                     'response': function(request){
                         request.respond(JSON.stringify({
-                            message: 'bonjour!'
+                            message: 'you got it'
                         }));
                     }
                 });
-                facade.get('bonjour')
-                    .then(function(response){
-                        expect(response.message).to.equal('bonjour!');
-                        done();
-                    });
+                facade.add({
+                    'name': 'method-man',
+                    'url': '/method-man',
+                    'type': 'POST',
+                    'response': function(request){
+                        request.respond(JSON.stringify({
+                            message: 'poster boy'
+                        }));
+                    }
+                });
+                facade.get([{
+                    'name': 'method-man',
+                    'type': 'GET'
+                }, {
+                    'name': 'method-man',
+                    'type': 'POST'
+                }]).spread(function(get, post){
+                    expect(get.message).to.equal('you got it');
+                    expect(post.message).to.equal('poster boy');
+                    done();
+                });
             });
         });
         describe('.get()', function() {
+
             it('should be a function', function() {
                 expect(facade.get).to.be.a('function');
             });
-            it('should return a promise', function() {
-                expect(facade.get() instanceof RSVP.Promise).to.be.true;
+
+            it('should throw an error when omitting request array', function(){
+                var err = {};
+                try{
+                    facade.get();
+                }catch(e){
+                    err = e;
+                }finally{
+                    expect(err.message).to.equal(XhrFacade.REQUEST_ARRAY_REQUIRED);
+                }
             });
+
+            it('should throw an error when omitting the URL while requesting an endpoint defined as a regular expresion', function(done){
+                var err = {};
+                try{
+                    facade.get(['custom']);
+                }catch(e){
+                    err = e;
+                }finally{
+                    expect(err.message).to.equal(XhrFacade.REGEXP_ENDPOINT_URL_REQUIRED);
+                    done();
+                }
+            });
+
+            it('should return a promise', function() {
+                expect(facade.get([]) instanceof RSVP.Promise).to.be.true;
+            });
+
             it('should allow calls to regular endpoints to pass through', function(done) {
                 facade
-                    .get({
+                    .get([{
                         'url': '/test/data/hello.json'
-                    })
-                    .then(function(response) {
-                        expect(response.message).to.equal('hello!');
+                    }])
+                    .then(function(responses) {
+                        expect(responses[0].message).to.equal('hello!');
                         done();
                     });
             });
+
             it('should return cached responses for duplicate requests', function(done) {
-                var options = {
-                    'name': 'hello',
-                    'url': '/test/data/hello.json'
-                };
-                RSVP.all([facade.get(options), facade.get(options)])
-                    .then(function(responses) {
+                RSVP.all([facade.get(['hola']), facade.get(['hola'])])
+                    .spread(function(first, second) {
                         try {
-                            expect(responses[0].message).to.equal('hello!');
-                            expect(responses[1].message).to.equal('hello!');
+                            expect(first[0].message).to.equal('hola!');
+                            expect(second[0].message).to.equal('hola!');
                             expect(jQuery.ajax.calledOnce).to.be.true;
                         } catch (e) {
                             done(e);
@@ -119,12 +195,45 @@
                         done();
                     });
             });
-            it('should pass extra parameters into resolve/reject callbacks', function(){
-                facade.get('virtual-service', 'extra', 'params')
-                    .then(function(response, extra, params){
-                        expect(response.message).to.equal("I'm a virtual response!");
+
+            it('should not return cached responses for calls to same endpoint when URL fragments differ', function(done){
+                RSVP.all([facade.get([{
+                    'name': 'custom',
+                    'url': '/custom/one'
+                }]), facade.get([{
+                    'name': 'custom',
+                    'url': '/custom/two'
+                }])]).spread(function(first, second){
+                    expect(first[0].message).to.equal('one');
+                    expect(second[0].message).to.equal('two');
+                    expect(jQuery.ajax.callCount).to.equal(2);
+                    done();
+                });
+            });
+
+            it('should pass extra parameters into resolve/reject callbacks', function(done){
+                facade.get(['bonjour'], 'extra', 'params')
+                    .spread(function(french, extra, params){
+                        expect(french.message).to.equal("bonjour!");
                         expect(extra).to.equal('extra');
                         expect(params).to.equal('params');
+                        done();
+                    });
+            });
+
+            it('calls can be chained', function(done){
+                facade.get(['hola'])
+                    .spread(function(spanish){
+                        return facade.get(['bonjour'], spanish);
+                    })
+                    .spread(function(french, spanish){
+                        return facade.get([{
+                            'name': 'custom',
+                            'url': '/custom/' + spanish.message + ' and ' + french.message
+                        }]);
+                    })
+                    .spread(function(response){
+                        expect(response.message).to.equal('hola! and bonjour!');
                         done();
                     });
             });
